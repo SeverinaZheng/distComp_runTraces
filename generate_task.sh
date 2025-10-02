854a873d5f17de199de048f46ee51439d7be07bf
YJZheng <YJZheng@node0.yjzheng-272692.cache-pg0.clemson.cloudlab.us>
2025-10-02 14:40:35 -0400

    redirect the stderr, not to go to pssh

--- generate_task.sh
+++ generate_task.sh
@@ -3,7 +3,7 @@
 # Find all .oracleGeneral.zst files under /mntData2/data/oracleReuse/ and generate cachesim commands
 find /mntData2/data/oracleReuse/ -name "*.oracleGeneral.zst" -type f | while read -r file; do
     absolute_path=$(realpath "$file")
-    echo "shell:4:2:2:/users/YJZheng/libCacheSim/_build/bin/cachesim $absolute_path oracleGeneral fifo,lru,clock,lfu,random,car,arc,lirs 0.001,0.01,0.1 --ignore-obj-size 0"
-    echo "shell:4:2:2:/users/YJZheng/libCacheSim/_build/bin/cachesim $absolute_path oracleGeneral belady,sieve,lecar 0.001,0.01,0.1 --ignore-obj-size 0"
-    echo "shell:4:2:2:/users/YJZheng/libCacheSim/_build/bin/cachesim $absolute_path oracleGeneral lhd,hyperbolic,gdsf,wtinyLFU,twoq,slru,s3fifo 0.001,0.01,0.1 --ignore-obj-size 0"
+    echo "shell:4:2:2:/users/YJZheng/libCacheSim/_build/bin/cachesim $absolute_path oracleGeneral fifo,lru,clock,lfu,random,car,arc,lirs 0.001,0.01,0.1 --ignore-obj-size 0 2> log.err"
+    echo "shell:4:2:2:/users/YJZheng/libCacheSim/_build/bin/cachesim $absolute_path oracleGeneral belady,sieve,lecar 0.001,0.01,0.1 --ignore-obj-size 0 2> log.err"
+    echo "shell:4:2:2:/users/YJZheng/libCacheSim/_build/bin/cachesim $absolute_path oracleGeneral lhd,hyperbolic,gdsf,wtinyLFU,twoq,slru,s3fifo 0.001,0.01,0.1 --ignore-obj-size 0 2> log.err"
 done > ./task
